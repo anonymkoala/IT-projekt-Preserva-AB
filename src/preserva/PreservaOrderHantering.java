1e062021e317c1708a65d39304a62242e497ab8f
P <P@pars-air.lan>
2017-04-20 12:19:12 +0200

    Change filestructure

---
+++ src/preserva/PreservaOrderHantering.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preserva;
+
+
+import PreservaView.MainWindow;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author P
+ */
+public class PreservaOrderHantering {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow myFrame = new MainWindow();
+        myFrame.setTitle("Administrera jobb");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setLocationRelativeTo(null); //centrerad
+        myFrame.pack(); //anpassar JFrame dialogen till kontrollernas storlek
+        myFrame.setVisible(true);
+    }
+    
+}
