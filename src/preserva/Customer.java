bea998e06c163298c7a47863cd78f2bb84be8e83
Patrik Lundstrom <patrik.burea@gmail.com>
2017-04-20 13:32:18 +0200

    Kundklass

---
+++ src/preserva/Customer.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preserva;
+
+/**
+ *
+ * @author Patrik
+ */
+public class Customer 
+{
+  private String Name;
+  private String phoneNumber;
+  private String eMail;
+  private String adress;
+    
+  
+  
+  
+  public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+  
+  
+  
+}
