0fd7703d698f3e2f370b5a47275704e5d67e77d9
P <P@pars-air.lan>
2017-04-20 11:44:59 +0200

    Change file-structure

--- src/preserva/NewCustomer.java
+++ src/preserva/NewCustomer.java
@@ -143,6 +143,7 @@ public class NewCustomer extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new NewCustomer().setVisible(true);
+                System.out.println("Test");
             }
         });
     }
