78cab9662fa094d04c1943f116955dea5677d35f
P <P@pars-air.lan>
2017-04-21 00:22:12 +0200

    Added Order-class Added MainWindow buttons to OrderUI and CustomerUI

--- src/PreservaControl/OrderMngr.java
+++ src/PreservaControl/OrderMngr.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Preserva.model;
+package Preserva.control;
 
 import java.util.ArrayList;
 
